b7625f3316c601066eb445485592283a7322edfb
Mona <moniquenagel24.mn@gmail.com>
2021-12-02 14:29:28 +0200

    Added functional collapsible navbar.

--- src/js/main.js
+++ src/js/main.js
@@ -4,9 +4,7 @@
 
 
 
-/*==========================
-  NAVBAR
-============================*/
+
 
 
 
@@ -16,12 +14,57 @@
 
 
 
-/*==========================
-  SCROLL TO TOP
-============================*/
+
 
 
 
 /*==========================
   CAROUSEL
 ============================*/
+
+
+(function() {
+  "use strict"; // Start of use strict
+
+  /*==========================
+    NAVBAR
+  ============================*/
+  var navbarToggle = document.querySelector('.nav-toggle');
+  var navOverlay = document.querySelector('.nav-overlay');
+  
+  //toggle between opening and closing navabar
+  if (navbarToggle) {
+    navbarToggle.addEventListener('click', function(e) {
+      e.preventDefault();
+
+      navOverlay.classList.toggle('active');
+      navbarToggle.classList.toggle('active');
+      
+      var icon = navbarToggle.querySelector('.fa-bars, .fa-times');
+      
+      // switch between bars and cross icon when nav is toggled
+      if (icon) {
+        //open
+        if (icon.classList.contains('fa-times')) {
+          icon.classList.remove('fa-times');
+          icon.classList.add('fa-bars');
+          navOverlay.style.height = "0%";//hides nav menu
+        } else if (icon.classList.contains('fa-bars')) {
+          icon.classList.remove('fa-bars');
+          icon.classList.add('fa-times');
+          navOverlay.style.height = "100%";//shows nav menu
+        }
+      }
+
+    });
+  }
+
+
+  /*==========================
+    SCROLL FUNCTIONS
+  ============================*/
+  
+
+  
+
+})(); // End of use strict
