80b44c817ba8ee0b5adeff435b534bec48ea2712
Mona <moniquenagel24.mn@gmail.com>
2021-12-02 22:56:08 +0200

    Converted more components to vanilla html. Set flex container and auto margin to senter content.

--- src/js/main.js
+++ src/js/main.js
@@ -23,16 +23,16 @@
 ============================*/
 
 
-(function() {
+/*==========================
+  NAVBAR
+============================*/
+(function Navbar() {
   "use strict"; // Start of use strict
 
-  /*==========================
-    NAVBAR
-  ============================*/
   var navbarToggle = document.querySelector('.nav-toggle');
   var navOverlay = document.querySelector('.nav-overlay');
   
-  //toggle between opening and closing navabar
+  //toggle between opening and closing navbar
   if (navbarToggle) {
     navbarToggle.addEventListener('click', function(e) {
       e.preventDefault();
@@ -44,27 +44,53 @@
       
       // switch between bars and cross icon when nav is toggled
       if (icon) {
-        //open
-        if (icon.classList.contains('fa-times')) {
+
+        if (icon.classList.contains('fa-times')) /*open*/{
+
           icon.classList.remove('fa-times');
           icon.classList.add('fa-bars');
+          if(icon.classList.contains('yellow-text')){
+            icon.classList.remove('yellow-text');
+            icon.classList.add('cyan-text');
+          }    
           navOverlay.style.height = "0%";//hides nav menu
-        } else if (icon.classList.contains('fa-bars')) {
+
+        } else if (icon.classList.contains('fa-bars')) /*close*/ {
+
           icon.classList.remove('fa-bars');
           icon.classList.add('fa-times');
+          if(icon.classList.contains('cyan-text')){
+            icon.classList.remove('cyan-text');
+            icon.classList.add('yellow-text');
+          }  
           navOverlay.style.height = "100%";//shows nav menu
         }
       }
 
     });
   }
+})(); // End of use strict
 
+/*==========================
+  SCROLL FUNCTIONS
+============================*/
+(function ScrollToTop() {
+  "use strict"; // Start of use strict
 
-  /*==========================
-    SCROLL FUNCTIONS
-  ============================*/
+  // Scroll to top button appear
+  var scrollToTop = document.querySelector('.scroll-to-top');
+  if (scrollToTop) {
   
+    // Scroll to top button appear
+    window.addEventListener('scroll', function() {
+      var scrollDistance = window.pageYOffset;
 
-  
+      if (scrollDistance > 100) {
+        scrollToTop.style.display = 'block';
+      } else {
+      scrollToTop.style.display = 'none';
+      }
+    });
+  }
 
 })(); // End of use strict
